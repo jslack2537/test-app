81ec2ff18f1db737af4b3746105087bb893f11f2
jslack2537 <jslack2537@gmail.com>
2020-03-25 18:18:00 +0000

    seed error still but json is parsing correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 encrypted_password = '#$taawktljasktlw4aaglj'
 file = File.read('db/people.json')
-data_hash = JSON.parse(file)
+data_hash = JSON.parse(file, symbolize_names: true)
 records = JSON.parse(File.read('db/people.json'))
-
-records.each do |record|
-    records['encrypted_password' => encrypted_password]
+records.each do |record| 
+    record['encrypted_password'] = encrypted_password
     user = User.create!(record.except('logins'))
     user.logins.create(record['logins'])
 
